2d2ae73517e56e6520ddcdbbc58e5a3725040a30
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-01-12 12:55:11 -0500

    hex clock finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,13 @@ function startClock() {
   var h = ("0" + d.getHours()).slice(-2);
   var m = ("0" + d.getMinutes()).slice(-2);
   var s = ("0" + d.getSeconds()).slice(-2);
-  var t = "#" + h + m + s;
-  document.getElementById("time").innerText = t;
-  document.getElementById("colors").style.backgroundColor = t;
+  var t = h + m + s;
+  document.getElementById("time").innerText = "#" + t;
+  document.getElementById("colors").style.backgroundColor = "#" + t;
+  var base = parseInt(t,16);
+  var fc = 1677215 - base;
+  var fc = ("0" + fc.toString(16)).slice(-6);
+  document.getElementById("time").style.color = "#" + fc;
   var r = setTimeout(startClock,1000)
 }
 
