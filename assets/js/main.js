ac207cdf870d5cffffab221ead3a933af85e9f46
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-01-12 11:58:49 -0500

    background color changes with time, elements are centered

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,9 @@ function startClock() {
   var h = ("0" + d.getHours()).slice(-2);
   var m = ("0" + d.getMinutes()).slice(-2);
   var s = ("0" + d.getSeconds()).slice(-2);
-  document.getElementById("time").innerText = "#" + h + m + s;
+  var t = "#" + h + m + s;
+  document.getElementById("time").innerText = t;
+  document.getElementById("colors").style.backgroundColor = t;
+  var r = setTimeout(startClock,1000)
 }
 
-function updateClock() {
-
-}
